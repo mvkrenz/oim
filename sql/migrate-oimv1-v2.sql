f31f13251b4bffe3a5f7c467468447d90b6aa024
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-03-26 18:41:23 +0000

    update some, and added insert statement for auth related tables

--- sql/migrate-oimv1-v2.sql
+++ sql/migrate-oimv1-v2.sql
@@ -1,5 +1,5 @@
 
-USE oim2;
+USE oimnew;
 
 INSERT INTO authorization_type (SELECT auth_type_id,description FROM oim.authorization_type);
 
@@ -30,20 +30,19 @@ INSERT INTO field_of_science (SELECT id,name FROM oim.field_of_science);
 
 INSERT INTO facility (SELECT facility_id,name,description,active,disable FROM oim.facility);
 
-INSERT INTO supportcenter (SELECT sc_id,short_name,long_name,description,community,active,disable,footprints_id FROM oim.supportcenter);
+INSERT INTO sc (SELECT sc_id,short_name,long_name,description,community,active,disable,footprints_id FROM oim.supportcenter);
 
-INSERT INTO virtualorganization 
+INSERT INTO vo
 	(SELECT vo_id,short_name,long_name,description,
 	primary_url,aup_url,membership_services_url,purpose_url,
 	support_url,app_description,community,sc_id,
-	active,disable,footprints_id 
+	active,disable,footprints_id
 	FROM oim.virtualorganization);
 
-INSERT INTO vo_vo (child_id, parent_id)
+INSERT INTO vo_vo (child_vo_id, parent_vo_id)
 	(SELECT vo_id, parent_vo_id
-	FROM oim.virtualorganization 
+	FROM oim.virtualorganization
 	WHERE parent_vo_id IS NOT NULL);
-
 
 INSERT INTO vo_field_of_science (SELECT vo_id,science_id FROM oim.vo_field_of_science);
 
@@ -52,12 +51,12 @@ INSERT INTO vo_report_name (SELECT id,reporting_name,vo_id FROM oim.vo_report_na
 INSERT INTO vo_fqan (SELECT id,fqan,vo_report_name_id FROM oim.vo_fqan);
 
 -- Insert person records without submiter_dn for now - will update later
-INSERT INTO person 
-	(id, first_name, middle_name, last_name, primary_email, secondary_email, 
-	primary_phone, primary_phone_ext, secondary_phone, secondary_phone_ext, 
+INSERT INTO person
+	(id, first_name, middle_name, last_name, primary_email, secondary_email,
+	primary_phone, primary_phone_ext, secondary_phone, secondary_phone_ext,
 	address_line_1, address_line_2, city, state, zipcode, country,
-	active, disable) 
-	(SELECT person_id, first_name, middle_name, last_name, primary_email, secondary_email, 
+	active, disable)
+	(SELECT person_id, first_name, middle_name, last_name, primary_email, secondary_email,
 	primary_phone, primary_phone_ext, secondary_phone, secondary_phone_ext, 
 	address_line_1, address_line_2, city, state, zipcode, cntry.name,
 	active, disable 
@@ -69,33 +68,30 @@ INSERT INTO person
 INSERT INTO mailing_list
 	(id, name, email)
 	(SELECT person_id, CONCAT(first_name, " ", last_name), primary_email
-	FROM oim.person pers 
+	FROM oim.person pers
 	WHERE pers.group_contact=1);
 
-INSERT INTO certificate_dn (id,dn_string,person_id) (SELECT dn_id, dn_string, person_id FROM oim.certificate_dn);
+INSERT INTO dn (id,dn_string,person_id) (SELECT dn_id, dn_string, person_id FROM oim.certificate_dn);
 -- UPDATE person SET optional_submitter_dn_id=(SELECT optional_submitter_dn_id FROM oim.person oldPerson WHERE oldPerson.person_id=person.id);
 
 INSERT INTO site (SELECT site_id,site.name,long_name,description,address_line_1,address_line_2,city,state,zipcode, Ctry.name, longitude,latitude,sc_id,facility_id,submitter_dn_id,active,disable FROM oim.site JOIN oim.country Ctry ON (Ctry.ccode=site.ccode));
 
 INSERT INTO resource_group (SELECT resource_group_id,name,description,site_id,osg_grid_type_id,active,disable FROM oim.resource_group);
 
-
-INSERT INTO resource 
-	(SELECT res.resource_id, name, description, fqdn, url, resExt.interop_bdii, 
-		resExt.interop_monitoring, resExt.interop_accounting, 
-		resExt.wlcg_accounting_name, active, disable ,1 
-	FROM oim.resource res 
+INSERT INTO resource
+	(SELECT res.resource_id, name, description, fqdn, url, resExt.interop_bdii,
+		resExt.interop_monitoring, resExt.interop_accounting,
+		resExt.wlcg_accounting_name, active, disable ,1
+	FROM oim.resource res
 	LEFT JOIN oim.resource_ext_attributes resExt ON (res.resource_id=resExt.resource_id));
 -- UPDATE resource res SET res.resource_group_id=(SELECT resource_group_id FROM oim.resource_resource_group resResGrp WHERE resResGrp.resource_id=res.id);
 
 INSERT INTO vo_resource_ownership (SELECT resource_id,vo_id,percent FROM oim.vo_resource_ownership);
 
-
-
 -- If need be
 -- SELECT dn_id, COUNT(person_id) FROM oim.dn_auth_type ORDER BY COUNT(person_id);
 -- DELETE FROM oim.dn_auth_type WHERE dn_id=65 AND person_id=0;
-INSERT INTO dn_auth_type (SELECT dn_id, auth_type_id, active FROM oim.dn_auth_type);
+INSERT INTO dn_authorization_type (SELECT dn_id, authorization_type_id FROM oim.dn_auth_type where active = 1);
 
 INSERT INTO resource_service (SELECT service_id, resource_id, uri, FALSE, FALSE FROM oim.resource_service);
 UPDATE resource_service SET central=TRUE WHERE service_id>=101 AND service_id<=106 ; -- Set central flag if service id > 100 < 107
@@ -103,7 +99,6 @@ UPDATE resource_service SET hidden=TRUE WHERE service_id=107;   -- Set hidden fl
 UPDATE resource_service SET service_id=1  WHERE service_id=107; -- only hidden CEs thus far AFAIK, so setting it to CE
 
 INSERT INTO resource_service_SE_details (SELECT resource_id,service_id,read_location,write_location,server_list_regex FROM oim.resource_service_SE_details);
-
 
 INSERT INTO resource_alias (resource_id, resource_alias) (SELECT resource_id, resource_alias FROM oim.resource_alias);
 
@@ -115,10 +110,41 @@ INSERT INTO vo_contact (SELECT person_id,vo_id,type_id,rank_id FROM oim.vo_conta
 
 INSERT INTO vo_report_contact (SELECT person_id,vo_report_name_id FROM oim.vo_report_contact);
 
-
 INSERT INTO resource_downtime (SELECT downtime_id,timestamp,start_time,end_time,downtime_summary,
 	downtime_class_id,downtime_severity_id,resource_id,dn_id,disable FROM oim.resource_downtime);
 
 INSERT INTO resource_downtime_service (SELECT downtime_id,service_id FROM oim.resource_downtime_service);
 
 INSERT INTO downtime_publish_wlcg (SELECT downtime_id,downtime_action_id,publish_status,timestamp,disable FROM oim.downtime_publish_wlcg);
+
+INSERT INTO action (id, name) values (1, "write_resource"),
+(2, "admin_resource"),
+(3, "write_facility"),
+(4, "admin_facility"),
+(5, "write_vo"),
+(6, "admin_vo"),
+(7, "write_sccontact"),
+(8, "admin_sccontact"),
+(9, "write_vocontact"),
+(10, "admin_vocontact"),
+(11, "write_sc"),
+(12, "admin_sc"),
+(13, "write_person"),
+(14, "admin_person");
+
+INSERT INTO `authorization_type_action` (`authorization_type_id`,`action_id`) VALUES
+ (4,1),
+ (4,2),
+ (4,3),
+ (4,4),
+ (4,5),
+ (4,6),
+ (4,7),
+ (4,8),
+ (4,9),
+ (4,10),
+ (4,11),
+ (4,12),
+ (4,13),
+ (4,14);
+
