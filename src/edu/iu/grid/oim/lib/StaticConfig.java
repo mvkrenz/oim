6b3d7232cd21f936bd70f0230d14ce356d1dd848
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-04-21 15:18:13 +0000

    2.18

--- src/edu/iu/grid/oim/lib/StaticConfig.java
+++ src/edu/iu/grid/oim/lib/StaticConfig.java
@@ -12,7 +12,7 @@ import org.apache.log4j.Logger;
 import org.xml.sax.InputSource;
 
 public class StaticConfig {
-	static public String getVersion() { return "2.17"; } //version shouldn't be configurable
+	static public String getVersion() { return "2.18"; } //version shouldn't be configurable
 	
 	static Logger log = Logger.getLogger(StaticConfig.class);  
     
