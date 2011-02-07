401f0c568d6150258e104fad69a586565588fe17
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2011-02-07 18:02:25 +0000

    Added more debug log for soap call for Footprint lib.

--- src/edu/iu/grid/oim/lib/Footprint.java
+++ src/edu/iu/grid/oim/lib/Footprint.java
@@ -539,8 +539,17 @@ public class Footprint
 	
 	void call() throws SOAPException
 	{
+        if(StaticConfig.isDebug()) {
+        	log.debug("Calling SOAP with following message");
+        	log.debug(msg);
+        }		
         SOAPMessage reply = connection.call(msg, StaticConfig.getFootprintsUrl());
         connection.close();
-        //return reply.getSOAPPart().getEnvelope().getBody();
+        
+        if(StaticConfig.isDebug()) {
+        	String out = reply.getSOAPPart().getEnvelope().getBody().toString();
+        	log.debug("Received following reply body");
+        	log.debug(out);
+        }
 	}
 }
