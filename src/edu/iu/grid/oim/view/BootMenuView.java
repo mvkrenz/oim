d7d1f1348075ed2b9c4af39ce94d9a6e3e5016bc
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2012-07-09 14:05:48 +0000

    Updated the menuview to display user account menu in button style - mimicking the latest bootstrap style.

--- src/edu/iu/grid/oim/view/BootMenuView.java
+++ src/edu/iu/grid/oim/view/BootMenuView.java
@@ -37,81 +37,34 @@ public class BootMenuView implements IView {
 		
 		out.println("<a class=\"brand\" href=\"http://opensciencegrid.org\"><img src=\"images/osglogo.40x30.png\"></a>");
 		
-		out.println("<div class=\"nav-collapse\">");
-		
-		//application menu
+		//application name & goc service list
 		out.println("<ul class=\"nav\">");
-		
 		out.println("<li class=\"dropdown\">");
-		out.println("<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">"+StaticConfig.getApplicationName()+" <b class=\"caret\"></b></a>");
-		out.println("<ul class=\"dropdown-menu\">");
-			out.println("<li><a href=\"https://myosg.grid.iu.edu\">MyOSG</a></li>");
-			out.println("<li class=\"active\"><a href=\"#\">OIM</a></li>");
-			out.println("<li><a href=\"https://ticket.grid.iu.edu\">Ticket</a></li>");
-			out.println("<li><a href=\"https://twiki.grid.iu.edu\">TWiki</a></li>");
-			out.println("<li class=\"divider\"></li>");
-			out.println("<li><a href=\"http://repo.grid.iu.edu\">Repo</a></li>");
-			out.println("<li><a href=\"http://software.grid.iu.edu\">Software Cache</a></li>");
-			out.println("<li><a href=\"http://is.grid.iu.edu\">OSG BDII</a></li>");
-			out.println("<li class=\"divider\"></li>");
-			out.println("<li><a href=\"http://blogs.grid.iu.edu\">Blog</a></li>");
-			out.println("<li><a href=\"http://display.grid.iu.edu\">Display</a></li>");
-			out.println("<li><a href=\"http://osggoc.blogspot.com/\">News</a></li>");
-		out.println("</ul>");
+			out.println("<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">"+StaticConfig.getApplicationName()+" <b class=\"caret\"></b></a>");
+			out.println("<ul class=\"dropdown-menu\">");
+				out.println("<li><a href=\"https://myosg.grid.iu.edu\">MyOSG</a></li>");
+				out.println("<li class=\"active\"><a href=\"#\">OIM</a></li>");
+				out.println("<li><a href=\"https://ticket.grid.iu.edu\">Ticket</a></li>");
+				out.println("<li><a href=\"https://twiki.grid.iu.edu\">TWiki</a></li>");
+				out.println("<li class=\"divider\"></li>");
+				out.println("<li><a href=\"http://repo.grid.iu.edu\">Repo</a></li>");
+				out.println("<li><a href=\"http://software.grid.iu.edu\">Software Cache</a></li>");
+				out.println("<li><a href=\"http://is.grid.iu.edu\">OSG BDII</a></li>");
+				out.println("<li class=\"divider\"></li>");
+				out.println("<li><a href=\"http://blogs.grid.iu.edu\">Blog</a></li>");
+				out.println("<li><a href=\"http://display.grid.iu.edu\">Display</a></li>");
+				out.println("<li><a href=\"http://osggoc.blogspot.com/\">News</a></li>");
+			out.println("</ul>");
 		out.println("</li>");
-		
-		out.println("<li class=\"divider-vertical\"></li>");
-		
-		if(current.equals("home")) {
-			out.println("<li class=\"active\"><a href=\"home\">Home</a></li>");
-		} else {
-			out.println("<li><a href=\"home\">Home</a></li>");	
-		}
-		if(current.equals("certificate")) {
-			out.println("<li class=\"active\"><a href=\"certificate\">Certificate</a></li>");
-		} else {
-			//for now, only show if it's debug
-			if(StaticConfig.isDebug()) {
-				out.println("<li><a href=\"certificate\">Certificate</a></li>");	
-			}
-		}
-		if(current.equals("topology")) {
-			out.println("<li class=\"active\"><a href=\"topology\">Topology</a></li>");
-		} else {
-			out.println("<li><a href=\"topology\">Topology</a></li>");	
-		}
-		
-		if(auth.isUser()) {
-			if(current.equals("resourcedowntime")) {
-				out.println("<li class=\"active\"><a href=\"resourcedowntime\">Downtimes</a></li>");
-			} else {
-				out.println("<li><a href=\"resourcedowntime\">Downtimes</a></li>");	
-			}
-		}
-	
-		if(current.equals("vo")) {
-			out.println("<li class=\"active\"><a href=\"vo\">Virtual Organizations</a></li>");
-		} else {
-			out.println("<li><a href=\"vo\">Virtual Organizations</a></li>");	
-		}
-		
-		if(current.equals("sc")) {
-			out.println("<li class=\"active\"><a href=\"sc\">Support Centers</a></li>");
-		} else {
-			out.println("<li><a href=\"sc\">Support Centers</a></li>");	
-		}
-				
-		out.println("</ul>");//end of left items
-		
+		out.println("</ul>");
 		
 		out.println("<ul class=\"nav pull-right\">");
-		
 		//account menu
 		if(auth.isUser()) {
 			ContactRecord contact = auth.getContact();
 			//user menu
-			out.println("<li class=\"dropdown\">");
-			out.println("<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">"+contact.name+" <b class=\"caret\"></b></a>");
+			out.println("<div class=\"btn-group\">");
+			out.println("<a href=\"#\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"icon-user\"></i> "+contact.name+" <span class=\"caret\"></span></a>");
 			out.println("<ul class=\"dropdown-menu\">");
 			
 				if(current.equals("profileedit")) {
@@ -189,7 +142,7 @@ public class BootMenuView implements IView {
 				out.println("<li><a href=\""+httpurl+"\">Logoff</a></li>");
 				
 			out.println("</ul>");//dropdown-menu
-			out.println("</li>");//dropdown
+			out.println("</div>");//btn-group
 		} else if(auth.isUnregistered()) {
 			if(current.equals("register")) {
 				out.println("<li class=\"active\">");
@@ -209,11 +162,59 @@ public class BootMenuView implements IView {
 		} else {
 			out.println("<li><a href=\""+StaticConfig.conf.getProperty("application.base")+"\">Login</a></li>");	
 		}
-		
-		
 		out.println("</ul>");//nav (pull-right)
 		
+		out.println("<div class=\"nav-collapse\">");
+		
+		out.println("<ul class=\"nav\">");
+		
+		//out.println("<li class=\"divider-vertical\"></li>");
+		
+		if(current.equals("home")) {
+			out.println("<li class=\"active\"><a href=\"home\">Home</a></li>");
+		} else {
+			out.println("<li><a href=\"home\">Home</a></li>");	
+		}
+		if(current.equals("certificate")) {
+			out.println("<li class=\"active\"><a href=\"certificate\">Certificate</a></li>");
+		} else {
+			//for now, only show if it's debug
+			if(StaticConfig.isDebug()) {
+				out.println("<li><a href=\"certificate\">Certificate</a></li>");	
+			}
+		}
+		if(current.equals("topology")) {
+			out.println("<li class=\"active\"><a href=\"topology\">Topology</a></li>");
+		} else {
+			out.println("<li><a href=\"topology\">Topology</a></li>");	
+		}
+		
+		if(auth.isUser()) {
+			if(current.equals("resourcedowntime")) {
+				out.println("<li class=\"active\"><a href=\"resourcedowntime\">Downtimes</a></li>");
+			} else {
+				out.println("<li><a href=\"resourcedowntime\">Downtimes</a></li>");	
+			}
+		}
+	
+		if(current.equals("vo")) {
+			out.println("<li class=\"active\"><a href=\"vo\">Virtual Organizations</a></li>");
+		} else {
+			out.println("<li><a href=\"vo\">Virtual Organizations</a></li>");	
+		}
+		
+		if(current.equals("sc")) {
+			out.println("<li class=\"active\"><a href=\"sc\">Support Centers</a></li>");
+		} else {
+			out.println("<li><a href=\"sc\">Support Centers</a></li>");	
+		}
+				
+		out.println("</ul>");//end of left items
+		
 		out.println("</div>");//nav-collaps	
+		
+	
+	
 		
 		out.println("</div>");//container-fluid
 		out.println("</div>");//navbar-inner
