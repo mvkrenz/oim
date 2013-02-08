44ebb10f334642c12ed6a3697974b96378de8ebb
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2013-02-08 15:06:09 +0000

    (patched) fixed the issue where only a few status is displayed in search form.

--- src/edu/iu/grid/oim/model/CertificateRequestStatus.java
+++ src/edu/iu/grid/oim/model/CertificateRequestStatus.java
@@ -14,33 +14,35 @@ public class CertificateRequestStatus {
 	public final static String FAILED = "FAILED";
 	
 	public final static int toInt(String status) {
+		//!!The number needs to be continous in order for search form to work 
 		if(status.equals(REQUESTED)) return 0;
 		if(status.equals(REVOCATION_REQUESTED)) return 1;
 		//if(status.equals(RENEW_REQUESTED)) return 2;
-		if(status.equals(APPROVED)) return 3;
-		if(status.equals(REJECTED)) return 4;
-		if(status.equals(CANCELED)) return 5;
-		if(status.equals(ISSUING)) return 6;
-		if(status.equals(ISSUED)) return 7;
-		if(status.equals(EXPIRED)) return 8;
-		if(status.equals(REVOKED)) return 9;
-		if(status.equals(FAILED)) return 10;
+		if(status.equals(APPROVED)) return 2;
+		if(status.equals(REJECTED)) return 3;
+		if(status.equals(CANCELED)) return 4;
+		if(status.equals(ISSUING)) return 5;
+		if(status.equals(ISSUED)) return 6;
+		if(status.equals(EXPIRED)) return 7;
+		if(status.equals(REVOKED)) return 8;
+		if(status.equals(FAILED)) return 9;
 		
 		return -1;
 	}
 	public final static String toStatus(int i) {
+		//!!The number needs to be continous in order for search form to work 
 		switch(i) {
 		case 0: return REQUESTED;
 		case 1: return REVOCATION_REQUESTED;
 		//case 2: return RENEW_REQUESTED;
-		case 3: return APPROVED;
-		case 4: return REJECTED;
-		case 5: return CANCELED;
-		case 6: return ISSUING;
-		case 7: return ISSUED;
-		case 8: return EXPIRED;
-		case 9: return REVOKED;
-		case 10: return FAILED;
+		case 2: return APPROVED;
+		case 3: return REJECTED;
+		case 4: return CANCELED;
+		case 5: return ISSUING;
+		case 6: return ISSUED;
+		case 7: return EXPIRED;
+		case 8: return REVOKED;
+		case 9: return FAILED;
 		}
 		return null;
 	}
