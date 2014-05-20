c764a7b90390874d7ad8dedbfc93728f9df7d603
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Michel C. Tavares 1397 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Michel C. Tavares 1397@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2014-05-20 18:52:25 +0000

    MT: Test case for Jira OIM-82

---
+++ src/edu/iu/grid/oim/test/CNValidatorTest.java
@@ -0,0 +1,39 @@
+package edu.iu.grid.oim.test;
+
+import static org.junit.Assert.*;
+/**
+ * MT
+ * Unit Test Cases for CNValidator
+ */
+
+import org.junit.Test;
+import edu.iu.grid.oim.view.divrep.form.validator.CNValidator;
+
+public class CNValidatorTest {
+
+	@Test
+	public final void testIsValidCertificate() {
+		CNValidator cnv = new CNValidator();
+		
+		// List of valid certificates
+		String[] validCertNames = {"iu.edu", "*.edu", "www.something.com"};
+		
+		for (String validCertName : validCertNames) 	{
+			
+			assertTrue("Certificate >" + validCertName + "< is valid", cnv.isValid(validCertName));	
+		}
+	}
+	
+	@Test
+	public final void testIsInvalidCertificate() {
+		CNValidator cnv = new CNValidator();
+		
+		// List of invalid certificates
+		String[] invalidCertNames = {".iu.edu", "/edu"};
+		
+		for (String invalidCertName : invalidCertNames) 	{
+			
+			assertFalse("Certificate >" + invalidCertName + "< is invalid", cnv.isValid(invalidCertName));	
+		}
+	}
+}
