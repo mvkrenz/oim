2dfe59ce2b685d0a6cf8498500544a6d1a1530dc
soichih <hayashis@iu.edu>
2014-07-09 15:10:45 -0400

    Added SAM_URI for CE service UI (OIM-97)

--- src/edu/iu/grid/oim/view/divrep/form/servicedetails/GridServiceDetails.java
+++ src/edu/iu/grid/oim/view/divrep/form/servicedetails/GridServiceDetails.java
@@ -15,6 +15,7 @@ public class GridServiceDetails extends ServiceDetailsContent {
 
 	private DivRepCheckBox hidden;
 	private DivRepTextBox uri_override;
+	private DivRepTextBox sam_uri;
 	
 	public GridServiceDetails(DivRep _parent, ServiceRecord srec) {
 		super(_parent);
@@ -25,7 +26,6 @@ public class GridServiceDetails extends ServiceDetailsContent {
 		uri_override = new DivRepTextBox(this);
 		uri_override.setLabel("Service URI Override");
 		uri_override.setSampleValue("some.host.edu:"+srec.port);
-		//uri_override.setRequired(true);
 		uri_override.addValidator(new DivRepIValidator<String>() {
 			String message;
 			public String getErrorMessage() {
@@ -38,15 +38,7 @@ public class GridServiceDetails extends ServiceDetailsContent {
 					message = "Please enter override in the form such as \"resource123.iu.edu:2119\"";
 					return false;
 				}
-				/* -- not sure we have this validation
-				//validate the url
-				String url = parts[0];
-				if(!parent.isValidResourceFQDN(url)) {
-					message = "Please use FQDN of this resource, or one of FQDN aliases.";
-					return false;
-				}
-				*/
-				
+			
 				//validate port
 				if(parts.length == 2) {
 					try {
@@ -58,7 +50,26 @@ public class GridServiceDetails extends ServiceDetailsContent {
 				}
 				
 				return true;
-			}});
+			}
+		});
+		
+		if(srec.id.equals(1)) { //only for CE
+			sam_uri = new DivRepTextBox(this);
+			sam_uri.setLabel("SAM URI");
+			sam_uri.setSampleValue("htcondor://$HOSTNAME:$PORT/$SCHEDDNAME?option1=val1&option2=val2");
+		
+			//TODO - add validation based on Brian's recommendation
+			/*
+			HOSTNAME: Regexp: (?:(?:(?:(?:[a-zA-Z0-9][-a-zA-Z0-9]{0,61})?[a-zA-Z0-9])[.])*(?:[a-zA-Z][-a-zA-Z0-9]{0,61}[a-zA-Z0-9]|[a-zA-Z])[.]?)
+			  - (just a regex for valid hostnames from http://stackoverflow.com/questions/1418423/the-hostname-regex)
+			
+			SCHEDDNAME: traditionally a hostname, but actually can be any valid string.  I'd suggest the following regexp: [-A-Za-z0-9.@_]{0,128}
+			
+			PORT: Optional (defaults to 9619), but should be a valid number: [0-9]{1,5}
+			
+			Options: Should match [-A-Za-z0-9_]+
+			 */
+		}
 	}
 	
 	public void setValues(HashMap<String, String> values) {
@@ -70,13 +81,22 @@ public class GridServiceDetails extends ServiceDetailsContent {
 		if(values.containsKey("uri_override")) {
 			uri_override.setValue(values.get("uri_override"));
 		}
+		if(sam_uri != null && values.containsKey("sam_uri")) {
+			sam_uri.setValue(values.get("sam_uri"));
+		}
 	}
 	
 	@Override
 	public void render(PrintWriter out) {
 		out.write("<div id=\""+getNodeID()+"\">");
 		uri_override.render(out);
+		
+		if(sam_uri != null) {
+			out.write("<p class=\"muted pull-right\">* Only For HTCondorCE</p>");
+			sam_uri.render(out);
+		}
 		hidden.render(out);
+		
 		out.write("</div>");
 	}
 
@@ -96,12 +116,20 @@ public class GridServiceDetails extends ServiceDetailsContent {
 		}
 		
 		map.put("uri_override", uri_override.getValue());
+		if(sam_uri != null) {
+			map.put("sam_uri", sam_uri.getValue());	
+		}
 		
 		return map;
 	}
 
 	@Override
 	public boolean validate() {
-		return uri_override.validate();
+		boolean valid = true;
+		valid &= uri_override.validate();
+		if(sam_uri != null) {
+			valid &= sam_uri.validate();
+		}
+		return valid;
 	}
 }
