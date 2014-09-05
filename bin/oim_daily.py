c04f49a0f70f336ba0ed098c4aed00dc914764f5
Soichi Hayashi <soichih@gmail.com>
2014-09-05 18:58:27 +0000

    applied the same patch made on oim_weekly

--- bin/oim_daily.py
+++ bin/oim_daily.py
@@ -9,16 +9,24 @@ conn = httplib.HTTPConnection("localhost")
 conn.request("POST", "/oim/rest?action=reset_daily_quota&version=1")
 response = conn.getresponse()
 if response.status != 200:
-	print "Failed to load quota_info from OIM. response.status: ",response.status,response.reason
+	print "reset_daily_quota (request) failed:",response.status,response.reason
 	sys.exit(response.status)
-#data = json.JsonReader().read(response.read())
+else:
+	data = json.loads(response.read())
+	status = data["status"]
+	if status != "OK":
+		print "reset_daily_quota failed:", data["detail"]
 
 #process expired certs
 conn.request("POST", "/oim/rest?action=find_expired_cert_request&version=1")
 response = conn.getresponse()
 if response.status != 200:
-	print "Failed to load quota_info from OIM. response.status: ",response.status,response.reason
+	print "find_expired_cert_request (request) failed: ",response.status,response.reason
 	sys.exit(response.status)
-#data = json.JsonReader().read(response.read())
+else:
+	data = json.loads(response.read())
+	status = data["status"]
+	if status != "OK":
+		print "find_expired_cert_request failed:", data["detail"]
 
 conn.close()
--- bin/oim_yearly.py
+++ bin/oim_yearly.py
@@ -9,8 +9,12 @@ conn = httplib.HTTPConnection("localhost")
 conn.request("POST", "/oim/rest?action=reset_yearly_quota&version=1")
 response = conn.getresponse()
 if response.status != 200:
-	print "Failed to load quota_info from OIM. response.status: ",response.status,response.reason
+	print "reset_yearly_quota (request) failed:",response.status,response.reason
 	sys.exit(response.status)
-#data = json.JsonReader().read(response.read())
+else:
+	data = json.loads(response.read())
+	status = data["status"]
+	if status != "OK":
+		print "reset_yearly_quota failed:", data["detail"]
 
 conn.close()
