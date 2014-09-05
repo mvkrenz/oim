e1580736485fe1c182dffe084d5888a88672f88c
Soichi Hayashi <soichih@gmail.com>
2014-09-05 14:45:47 -0400

    Added check for real service return code

--- bin/oim_weekly.py
+++ bin/oim_weekly.py
@@ -4,11 +4,16 @@ import httplib
 import json
 import sys
 
-conn = httplib.HTTPConnection("localhost")
+conn = httplib.HTTPConnection("t520", 8080)
 conn.request("POST", "/oim/rest?action=notify_expiring_cert_request&version=1")
 response = conn.getresponse()
 if response.status != 200:
         print "Failed to run notify_expiring_cert_request ito OIM. response.status: ",response.status,response.reason
         sys.exit(response.status)
-conn.close()
+else:
+	data = json.loads(response.read())
+	status = data["status"]
+	if status != "OK":
+		print "notify_expiring_cert_request failed:", data["detail"]
 
+conn.close()
