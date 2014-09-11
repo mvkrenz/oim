3514a7a822eecde883a8d3a2e0c2990b7f1660d5
Soichi Hayashi <soichih@gmail.com>
2014-09-11 21:03:55 +0000

    added 3.36 migration sql

---
+++ sql/3.36.sql
@@ -0,0 +1,2 @@
+alter table mesh_config_test change groupa_id groupa_ids varchar(1024);
+alter table mesh_config_test change groupb_id groupb_ids varchar(1024);
---
+++ sql/reset.sh
@@ -0,0 +1,7 @@
+echo "TRUNCATE mesh_config_group" | mysql -u root oim
+echo "TRUNCATE mesh_config_oim_member" | mysql -u root oim
+echo "TRUNCATE mesh_config_wlcg_member" | mysql -u root oim
+echo "TRUNCATE mesh_config_param" | mysql -u root oim
+echo "TRUNCATE mesh_config_test" | mysql -u root oim
+echo "TRUNCATE mesh_config_contact" | mysql -u root oim
+echo "TRUNCATE mesh_config" | mysql -u root oim
