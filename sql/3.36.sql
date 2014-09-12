fc3c5ad676c7d8ad555bc3143db1b08ccf4ddee9
Soichi Hayashi <soichih@gmail.com>
2014-09-12 21:34:18 +0000

    added new oasis_repo_urls field in VO

--- sql/3.36.sql
+++ sql/3.36.sql
@@ -1,2 +1,4 @@
 alter table mesh_config_test change groupa_id groupa_ids varchar(1024);
 alter table mesh_config_test change groupb_id groupb_ids varchar(1024);
+
+alter table vo add oasis_repo_urls text;
