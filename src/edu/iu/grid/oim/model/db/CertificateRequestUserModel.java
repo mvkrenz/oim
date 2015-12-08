5e757dcd4d7cd8960114c29baf754a801db5ac77
Soichi Hayashi <hayashis@soichi7.ppa.iu.edu>
2015-12-08 16:02:35 +0000

    Revert "Merge remote-tracking branch 'origin/ATLAS3'"
    This reverts commit 1eb4c97ada6d7a3686e6105ec3336c84b80fcf97, reversing
    changes made to 2a8dd054a80b3c8480cc30b4b00a59191f07c2e3.

--- src/edu/iu/grid/oim/model/db/CertificateRequestUserModel.java
+++ src/edu/iu/grid/oim/model/db/CertificateRequestUserModel.java
@@ -1073,14 +1073,8 @@ public class CertificateRequestUserModel extends CertificateRequestModelBase<Cer
 				
 				//send notification
 				ticket.description = "Dear " + requester.name + ",\n\n";
-                       		 if(rec.vo_id == 35) { //ATLAS for OSGPKI-399
-                                	ticket.description += "In 2016, US ATLAS will cease to use OSG-supplied certificates, in favor of certificates issued by the CERN certificate authority (CA). ";
-                                	ticket.description += "To ensure minimal impact to your ATLAS privileges through this transition, US ATLAS encourages you to obtain a user grid certificate from the CERN CA (if you don't yet have one), and add it to your existing ATLAS VO membership.\n\n";
-                                	ticket.description += "For details on the OSG certificate CA migration, visit https://www.racf.bnl.gov/docs/howto/grid/osg-ca-migration.";
-                        	}	
-				else {
-					ticket.description += "Your user certificate ("+rec.dn+") has expired. Please re-request your certificate at "+getTicketUrl(rec.id, TabLabels.re_request);
-				}	
+				ticket.description += "Your user certificate ("+rec.dn+") has expired. Please re-request your certificate at "+getTicketUrl(rec.id, TabLabels.re_request);
+				
 				if(StaticConfig.isDebug()) {
 					log.debug("skipping (this is debug) ticket update on ticket : " + rec.goc_ticket_id + " to notify expired user certificate");
 					log.debug(ticket.description);
@@ -1201,20 +1195,12 @@ public class CertificateRequestUserModel extends CertificateRequestModelBase<Cer
 				ticket.description += "In 2016, US ATLAS will cease to use OSG-supplied certificates, in favor of certificates issued by the CERN certificate authority (CA). ";
 				ticket.description += "To ensure minimal impact to your ATLAS privileges through this transition, US ATLAS encourages you to obtain a personal certificate from the CERN CA (if you don't yet have one), and add it to your existing ATLAS VO membership.\n\n";
 				ticket.description += "For details on the OSG certificate CA migration, click https://www.racf.bnl.gov/docs/howto/grid/osg-ca-migration\n";
-				ticket.description += "To ensure minimal impact to your ATLAS privileges through this transition, US ATLAS encourages you to obtain a user grid certificate from the CERN CA (if you don't yet have one), and add it to your existing ATLAS VO membership.\n\n";
-				ticket.description += "For details on the OSG certificate CA migration, click https://www.racf.bnl.gov/docs/howto/grid/osg-ca-migration\n";
+			}
+			if(criterias.passAll()) {	
+				ticket.description += "Please renew your user certificate at "+getTicketUrl(rec.id, TabLabels.renew)+"\n\n";
+				ticket.status = "Engineering"; //reopen it - until user renew
 			} else {
-				if(criterias.passAll()) {	
-					ticket.description += "Please renew your user certificate at "+getTicketUrl(rec.id, TabLabels.renew)+"\n\n";
-					ticket.status = "Engineering"; //reopen it - until user renew
-				} else {
-					ticket.description += "Please request new user certificate by visiting https://oim.grid.iu.edu/oim/certificaterequestuser\n\n";
-				}
-				//OSGPKI-393 (updated to put this under both cases)
-				ticket.description += "Note: Check to make sure that your soon to expire DigiCert user certificate is currently installed on your browser. ";
-				ticket.description += "Your full name should show up in the upper right-hand corner of the OIM page. ";
-				ticket.description += "If it is not, then please install your DigiCert user certificate and restart the browser. ";
-				ticket.description += "PKI renewal documentation can be found here: https://twiki.grid.iu.edu/bin/view/Documentation/OSGPKICertificateRenewal\n\n";
+				ticket.description += "Please request for new user certificate by visiting https://oim.grid.iu.edu/oim/certificaterequestuser\n\n";
 			}
 
 			//OSGPKI-393 (updated to put this under both cases)
