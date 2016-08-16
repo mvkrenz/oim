20e419fe617d7fcf69fc3c842af9b3e5a3c49d4a
mvkrenz <mvkrenz@iu.edu>
2016-08-16 12:36:54 -0400

    changed second revoke function
    Changed second revoke function

--- src/edu/iu/grid/oim/model/db/CertificateRequestHostModel.java
+++ src/edu/iu/grid/oim/model/db/CertificateRequestHostModel.java
@@ -1046,7 +1046,34 @@ public class CertificateRequestHostModel extends CertificateRequestModelBase<Cer
 		}
 		
 		//revoke one
-		CertificateManager cm = CertificateManager.Factory(context, rec.approver_vo_id);
+	//	CertificateManager cm = CertificateManager.Factory(context, rec.approver_vo_id);
+	// beginning of change for 8/16/2016
+
+		String issuer_dn = ""; //by default
+		ArrayList<Certificate> chain = null;
+		try {
+			if (rec.getPKCS7s()[0] != null) {
+				chain = CertificateManager.parsePKCS7(rec.getPKCS7s()[0]);
+				X509Certificate c0 = CertificateManager.getIssuedX509Cert(chain);
+				X500Principal issuer = c0.getIssuerX500Principal();
+				issuer_dn = CertificateManager.X500Principal_to_ApacheDN(issuer);
+				log.debug("issuer dn is " + issuer_dn);
+			}
+		} catch (CertificateException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (CMSException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			//e2.printStackTrace(); -this will error for cilogon
+			
+		}
+
+
+		CertificateManager cm = CertificateManager.Factory(issuer_dn);
+		// end of change 8/16/2016
 		try {
 			log.info("Revoking certificate with serial ID: " + cert_serial_id);
 			cm.revokeHostCertificate(cert_serial_id);
